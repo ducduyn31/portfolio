c06b519a87883974ab8de69157f91db2fffaa5db
Daniel Ng <ducduy.ababat@gmail.com>
2020-02-26 09:18:28 -0600

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import VueFullPage from 'vue-fullpage.js/dist/vue-fullpage'
+import 'fullpage.js/vendors/scrolloverflow'
+import 'fullpage.js/dist/fullpage.css'
+
+Vue.config.productionTip = false;
+
+Vue.use(VueFullPage);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
